58df8e2229a5f64a2adc95e4cd6598d10739dca1
Ejaz Khan <46069373+Ejazkhan42@users.noreply.github.com>
2023-07-18 21:21:58 +0500

    API

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ except KeyError:
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    r = requests.get('https://api.publicapis.org/entries')
     if r.status_code == 200:
         data = r.json()
-        temperature = data["forecast"]["temp"]
+        temperature = data["entries"][0]["API"]
         logger.info(f'Weather in Berlin: {temperature}')
