bbf06b90ad867fa39aca425814f1ba96fbf5c90a
Swapnanil Sharma <swapnanilsharma@gmail.com>
2024-05-05 22:27:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,28 +39,29 @@ except KeyError:
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
     city = "Pune"
-
-    r = requests.get(f"https://weather.talkpython.fm/api/weather/?city={city}&country=IN")
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        feels_like = data["forecast"]["feels_like"]
-        humidity = data["forecast"]["humidity"]
-
-        current_time = datetime.now()
-        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-
-
-        # Get the current time in UTC
-        utc_now = datetime.utcnow()
-        
-        # Convert UTC time to IST
-        ist = pytz.timezone('Asia/Kolkata')
-        ist_now = utc_now.astimezone(ist)
-        
-        # Format and print the IST time
-        logger.info(f"Current IST time:", {ist_now.strftime("%Y-%m-%d %H:%M:%S")})
-
-        logger.info(
-            f"Temperature in {city}: {temperature}, it feels like: {feels_like}, with Humidity: {humidity}"
-        )
+    try:
+        r = requests.get(f"https://weather.talkpython.fm/api/weather/?city={city}&country=IN")
+        if r.status_code == 200:
+            data = r.json()
+            temperature = data["forecast"]["temp"]
+            feels_like = data["forecast"]["feels_like"]
+            humidity = data["forecast"]["humidity"]
+    
+            current_time = datetime.now()
+            formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    
+            # Get the current time in UTC
+            utc_now = datetime.utcnow()
+            
+            # Convert UTC time to IST
+            ist = pytz.timezone('Asia/Kolkata')
+            ist_now = utc_now.astimezone(ist)
+            
+            # Format and print the IST time
+            logger.info(f"Current IST time: {ist_now.strftime("%Y-%m-%d %H:%M:%S")}")
+    
+            logger.info(
+                f"Temperature in {city}: {temperature}, it feels like: {feels_like}, with Humidity: {humidity}"
+            )
+    except:
+        logger.info("API timeout! Check after sometime.")
