190ff04d88b7d58c0a600f26b13dae3164d174d0
Swapnanil Sharma <swapnanilsharma@gmail.com>
2024-05-05 18:45:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,5 @@ if __name__ == "__main__":
         formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
         logger.info(
-            f"Temperature in {city}: {temperature}, \
-            it feels like: {feels_like}, \
-            with Humidity: {humidity}"
+            f"Temperature in {city}: {temperature}, it feels like: {feels_like}, with Humidity: {humidity}"
         )
